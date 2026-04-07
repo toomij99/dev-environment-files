1e4fc2e97e31aafc7b31588c7eae19999bd20779
toomij99 <at@aisec.co.il>
2026-04-07 16:59:37 +0300

    Add Python build dependencies for pyenv

--- install.sh
+++ install.sh
@@ -348,6 +348,19 @@ install_packages_apt() {
         "zoxide"
         "zsh-autosuggestions"
         "unzip"
+        "build-essential"
+        "libssl-dev"
+        "zlib1g-dev"
+        "libbz2-dev"
+        "libreadline-dev"
+        "libsqlite3-dev"
+        "libncursesw5-dev"
+        "xz-utils"
+        "tk-dev"
+        "libxml2-dev"
+        "libxmlsec1-dev"
+        "libffi-dev"
+        "liblzma-dev"
     )
 
     sudo apt-get update
